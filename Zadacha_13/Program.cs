56558a7b317308776f87590eb2d65701713721b6
Yujev <ustatar@ya.ru>
2022-09-22 18:48:28 +0300

    Решил задачу 13 с вопросами...

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -6,3 +6,15 @@
 
 32679 -> 6
 */
+Console.WriteLine("Введите любое число: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = ((number1 / 100) % 10);
+
+if (number2 != 0)
+{
+    Console.WriteLine($"Третья цифра этого числа: {number2}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
