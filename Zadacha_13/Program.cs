43f3642134769b22e3b9eb73b55bcaebe9b793e0
Yujev <ustatar@ya.ru>
2022-09-27 17:47:51 +0300

    Добавил решение от Софьи

--- Zadacha_13/Program.cs
+++ Zadacha_13/Program.cs
@@ -1,4 +1,5 @@
-﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
 
 645 -> 5
 
@@ -6,6 +7,7 @@
 
 32679 -> 6
 */
+/*Моё решение
 Console.WriteLine("Введите любое число: ");
 int number1 = int.Parse(Console.ReadLine());
 int number2 = ((number1 / 100) % 10);
@@ -17,4 +19,38 @@ if (number2 != 0)
 else
 {
     Console.WriteLine($"Третьей цифры нет");
+}
+*/
+// Решение от Софьи:
+
+Console.WriteLine("Выбери вариант решения: ");
+int chosenNumber = int.Parse(Console.ReadLine());
+
+if (chosenNumber == 1)
+{
+    Console.Write("Введи число: ");
+    int num = int.Parse(Console.ReadLine());
+    if (num < 100)
+    {
+        Console.WriteLine($"Третьей цифры нет");
+        return;
+    }
+    int thirdNumber = 0;
+    if (num > 99 && num < 1000)
+    {
+        thirdNumber = (num % 100) % 10;
+    }
+    if (num > 99 && num < 10000)
+    {
+        thirdNumber = (num % 100) % 10;
+    }
+    if (num > 99 && num < 100000)
+    {
+        thirdNumber = (num % 1000) % 100;
+    }
+    if (num > 99 && num < 1000000)
+    {
+        thirdNumber = (num % 10000) % 1000;
+    }
+    Console.WriteLine($"Третья цифра числа {num} -> {thirdNumber}");
 }
