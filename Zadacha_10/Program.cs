66b0ba4a1203bf632454930438d485c284a9340b
Yujev <ustatar@ya.ru>
2022-09-27 17:27:58 +0300

    Добавил решение от Софьи

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -15,3 +15,8 @@ int number3 = number2 / 10;
 
 Console.WriteLine($"Вторая цифра этого числа: {number3} ");
 
+/* Решение Софьи
+int num = new Random().Next(100, 1000);
+int secondNumber = num / 10%10;
+Console.WriteLine($"Вторая цифра в числе {num} -> {secondNumber}");
+*/
